c45bba1a4e75b7ff9e1acce71edc78eb2e3c035f
sea <sea11rod@gmail.com>
2024-09-07 23:10:39 +0530

    update content

--- static/js/script.js
+++ static/js/script.js
@@ -1,16 +1,23 @@
-var h1 = document.getElementById("animin");
-var msg = h1.innerHTML;
-h1.innerHTML = "";
-console.log();
+var h1 = document.getElementsByClassName("animin");
+var msg = [];
 
-document.getElementsByTagName("body")[0].onload = function animin_fun(){
-    var i = 0
-    setInterval(
-        ()=>{
-                if (i>= msg.length)
-                    return 0;
-                h1.innerHTML += msg[i];
-                i++;
-            },200
-        )
+for (let i = 0; i < h1.length; i++) {
+  msg.push(h1[i].innerHTML);
+  h1[i].innerHTML = "";
 }
+
+
+console.log(msg);
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+document.getElementsByTagName("body")[0].onload = async function animin_fun() {
+  for (let j = 0; j < msg.length; j++) {
+    for (let i = 0; i < msg[j].length; i++) {
+      h1[j].innerHTML += msg[j][i];
+      await sleep(100);
+    }
+  }
+};
