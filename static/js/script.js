9c56b8310ff30c195cd91ebae4d00f6f6195aea2
sea <sea11rod@gmail.com>
2024-09-11 12:38:31 +0530

    added animation cards

--- static/js/script.js
+++ static/js/script.js
@@ -22,3 +22,14 @@ document.getElementsByTagName("body")[0].onload = async function animin_fun() {
   }
 };
 
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) entry.target.classList.add("show");
+    else entry.target.classList.remove("show");
+  });
+});
+
+const hidden_elements = document.querySelectorAll(".hidden");
+hidden_elements.forEach((element) => {
+  observer.observe(element);
+});
